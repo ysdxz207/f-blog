7883cde09a48f2727257e711dacc4403b96c127b
ysdxz207 <ysdxz207@qq.com>
2017-08-14 22:32:14 +0800

    run

--- src/main/java/com/puyixiaowo/fblog/generator/Run.java
+++ src/main/java/com/puyixiaowo/fblog/generator/Run.java
@@ -1,7 +1,11 @@
 package com.puyixiaowo.fblog.generator;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -10,6 +14,12 @@ import java.sql.*;
  */
 public class Run {
     public static void main(String[] args) {
+        String tableName = "user";
+        String src = "src/main/java";
+        String domainPackage = "com.puyixiaowo.fblog.domain";
+
+
+        Map<String, String> tables = new HashMap<>();
         try {
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
@@ -21,19 +31,35 @@ public class Run {
                 Connection conn = DriverManager.getConnection("jdbc:sqlite:f_blog.db")) {
 
             try (Statement statement = conn.createStatement();
-                 ResultSet resultSet = statement.executeQuery("PRAGMA table_info([user])")
+                 ResultSet resultSet = statement.executeQuery("PRAGMA table_info([" + tableName + "])")
             ) {
 
                 while (resultSet.next()) {
-                    String tableName = resultSet.getString("name");
-
-                    System.out.println(tableName);
-
+                    String columnName = resultSet.getString("name");
+                    String type = resultSet.getString("type");
+                    tables.put(columnName, type);
                 }
+
+
+                String basePath = System.getProperty("user.dir").replaceAll("\\\\", "/");
+
+                String domainPath = basePath + "/"
+                        + src.replaceAll("\\\\",
+                        "/") + "/" + domainPackage
+                        .replaceAll("\\.", "/");
+
+                File file = new File(domainPath);
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             }
 
         } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.err.println(e.getClass().getName() + ":" + e.getMessage());
             System.exit(0);
         }
 
