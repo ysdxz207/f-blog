407f2d3ed28dc508e799d8c4446ab0e82318db23
Moses <Moses@lamic.cn>
2017-09-29 10:54:40 +0800

    :smile:

--- src/main/java/com/puyixiaowo/fblog/main/Main.java
+++ src/main/java/com/puyixiaowo/fblog/main/Main.java
@@ -3,13 +3,17 @@ package com.puyixiaowo.fblog.main;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.puyixiaowo.fblog.Routes;
 import com.puyixiaowo.fblog.bean.sys.AppConfigBean;
+import com.puyixiaowo.fblog.constants.Constants;
 import com.puyixiaowo.fblog.error.ErrorHandler;
 import com.puyixiaowo.fblog.generator.utils.CustomIdSerializer;
 import com.puyixiaowo.fblog.utils.AppUtils;
 import com.puyixiaowo.fblog.utils.ConfigUtils;
+import com.puyixiaowo.fblog.utils.IpUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static spark.Spark.before;
+import static spark.Spark.halt;
 import static spark.Spark.port;
 
 /**
@@ -21,6 +25,7 @@ public class Main {
     /**
      * 支持启动设置端口：java -jar f-blog-1.0.jar -p 1521,
      * 默认启动端口8003
+     *
      * @param args
      */
     public static void main(String[] args) {
