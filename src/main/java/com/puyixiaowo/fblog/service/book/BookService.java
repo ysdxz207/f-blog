595b3a71e3c347a03645a9478812f382e80cc762
ysdxz207 <ysdxz207@qq.com>
2017-12-20 13:56:45 +0800

    Update BookService.java

--- src/main/java/com/puyixiaowo/fblog/service/book/BookService.java
+++ src/main/java/com/puyixiaowo/fblog/service/book/BookService.java
@@ -52,7 +52,7 @@ public class BookService {
 
         List<BookBean> bookBeanList = DBUtils.selectList(BookBean.class,
                 "select * from book where " +
-                "id in (" + bookShelBean.getBookIds() + ')', params);
+                "id in (" + bookShelfBean.getBookIds() + ')', params);
         return bookBeanList;
     }
 }
